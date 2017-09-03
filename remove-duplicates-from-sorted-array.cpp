92d1bd62985739663c965d7a76faeffe5bceaa63
Amit Bansal <007amitbansal@gmail.com>
2017-09-03 13:33:49 +0530

    remove-duplicates-from-sorted-array

--- remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array.cpp
@@ -1,4 +1,5 @@
 //Runtime: 29 ms
+// O(n) memory, O(n) space
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -8,5 +9,45 @@ public:
         vector<int> q(S.begin(),S.end());
         nums = q;
         return S.size();
+    }
+};
+
+//Runtime: 49 ms
+// O(n) memory, O(1) space
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        vector<int>::iterator it;
+        int n = nums.size();
+        if( n== 0)
+            return 0;
+        for(it=nums.begin()+1;it!=nums.end();it++)
+            while(*it == *(it-1))
+            {
+                n--;
+                nums.erase(it-1);
+                if(it == nums.end())
+                    return n;
+            }
+        return n;
+    }
+};
+
+//Runtime: 28 ms
+// O(n) memory, O(1) space
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int n = nums.size();
+        if(n < 2) return n;
+        int idx = 1;
+
+        for(int i=1;i<n;i++)
+            if(nums[i] != nums[i-1])
+                nums[idx++] = nums[i];
+
+        return idx;
     }
 };
