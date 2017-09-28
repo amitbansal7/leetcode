6c725874f79b581eb423c74af06f70e79bfedcb3
Amit Bansal <007amitbansal@gmail.com>
2017-09-28 18:31:31 +0530

    116. Populating Next Right Pointers in Each Node

---
+++ populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,60 @@
+//Runtime: 26 ms
+class Solution {
+public:
+    void solve(TreeLinkNode* root,stack<TreeLinkNode*>S)
+    {
+        if(!root)
+            return;
+
+        else if(S.empty())
+            root->next = NULL;
+
+        else if(S.top()->left == root)
+            root->next = S.top()->right;
+
+        else
+        {
+            TreeLinkNode* tparent = S.top();
+            stack<TreeLinkNode*> tS;
+            tS.push(S.top());
+            S.pop();
+            int lvl = 1;
+
+            while(!S.empty() && S.top()->right == tparent)
+            {
+                tparent = S.top();
+                tS.push(tparent);
+                S.pop();
+                lvl++;
+            }
+            int mhe = 0;
+            TreeLinkNode* mainhead;
+            if(!S.empty())
+            {
+                mainhead = S.top()->right;
+                mhe = 1;
+            }
+            else
+                mainhead = NULL;
+
+            while(mhe && lvl--)
+                mainhead = mainhead->left;
+
+            root->next = mainhead;
+
+            while(!tS.empty())
+            {
+                S.push(tS.top());
+                tS.pop();
+            }
+        }
+
+        S.push(root);
+        solve(root->left, S);
+        solve(root->right, S);
+    }
+    void connect(TreeLinkNode *root) {
+    	stack<TreeLinkNode*> S;
+        solve(root, S);
+    }
+};
