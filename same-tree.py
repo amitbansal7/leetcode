a3fba1e21cbebda90b42537d1310c3ffaa6238e6
Amit Bansal <007amitbansal@gmail.com>
2018-01-16 19:15:20 +0530

    100. Same Tree

---
+++ same-tree.py
@@ -0,0 +1,10 @@
+# Runtime: 37 ms
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p is None and q is None:
+            return True
+
+        if p is None or q is None or p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
