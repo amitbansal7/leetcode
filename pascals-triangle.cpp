291da0618f2180c57accce24ad568af63996d181
Amit Bansal <007amitbansal@gmail.com>
2018-01-28 21:07:09 +0530

    118. Pascal's Triangle

---
+++ pascals-triangle.cpp
@@ -0,0 +1,18 @@
+//Runtime: 3 ms
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int> >res;
+
+        for(int i=0;i<numRows;i++){
+            vector<int> row(i+1);
+            row[0] = row[row.size()-1] = 1;
+
+            for(int j=1;j<i;j++){
+                row[j] = res[i-1][j-1] + res[i-1][j];
+            }
+            res.push_back(row);
+        }
+        return res;
+    }
+};
