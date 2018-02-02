2676e5236eda11dac463051081841bdc84652e48
Amit Bansal <007amitbansal@gmail.com>
2018-02-03 00:14:44 +0530

    417. Pacific Atlantic Water Flow

--- pacific-atlantic-water-flow.cpp
+++ pacific-atlantic-water-flow.cpp
@@ -53,3 +53,51 @@ public:
 		return res;
 	}
 };
+
+//Runtime: 47 ms
+class Solution {
+public:
+
+	void dfs(vector<vector<int> >&m, int i, int j, vector<vector<bool> >&vis, int prev) {
+		if (i < 0 || j < 0 || i >= m.size() || j >= m[0].size() || m[i][j] < prev || vis[i][j])
+			return;
+
+		vis[i][j] = 1;
+		prev = m[i][j];
+		m[i][j] = INT_MIN;
+		dfs(m, i + 1, j, vis, prev);
+		dfs(m, i - 1, j, vis, prev);
+		dfs(m, i, j + 1, vis, prev);
+		dfs(m, i, j - 1, vis, prev);
+		m[i][j] = prev;
+	}
+
+	vector<pair<int, int>> pacificAtlantic(vector<vector<int>>& matrix) {
+		vector<pair<int, int> >res;
+		int m = matrix.size();
+		if (m == 0)
+			return res;
+		int n = matrix[0].size();
+
+		vector<vector<bool> >pac(m, vector<bool>(n, 0));
+		vector<vector<bool> > atl = pac;
+
+		for (int i = 0; i < m; i++) {
+			dfs(matrix, i, 0, pac, INT_MIN);
+			dfs(matrix, i, n - 1, atl, INT_MIN);
+		}
+
+		for (int j = 0; j < n; j++) {
+			dfs(matrix, 0, j, pac, INT_MIN);
+			dfs(matrix, m - 1, j, atl, INT_MIN);
+		}
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (pac[i][j] && atl[i][j])
+					res.push_back(make_pair(i, j));
+			}
+		}
+		return res;
+	}
+};
