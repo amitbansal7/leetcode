146f4a343fe0dc7e7d31bfb24a10a0025a24214a
Amit Bansal <007amitbansal@gmail.com>
2018-04-07 17:32:15 +0530

    485. Max Consecutive Ones

--- max-consecutive-ones.scala
+++ max-consecutive-ones.scala
@@ -1,19 +1,11 @@
-//Runtime: 452 ms
+//Runtime: 464 ms
 object Solution {
   def findMaxConsecutiveOnes(nums: Array[Int]): Int = {
-    def count(i: Int, nums: Array[Int], acc: Int): Int = {
-      if (i >= nums.size || nums(i) == 0) acc
-      else count(i + 1, nums, acc + 1)
+    def solve(nums: Array[Int], i: Int, res: Int, acc: Int): Int = {
+      if (i >= nums.size) res max acc
+      else if (nums(i) == 0) solve(nums, i + 1, res max acc, 0)
+      else solve(nums, i + 1, res, acc + 1)
     }
-
-    def solve(nums: Array[Int], res: Int, i: Int): Int = {
-      if (i >= nums.size) res
-      else {
-        val c = count(i, nums, 0)
-        solve(nums, res max c, i + c + 1)
-      }
-    }
-
-    solve(nums, 0, 0)
+    solve(nums, 0, 0, 0)
   }
 }
