3d42741780a461f6d9846ee344f5cdb18e8c7798
Amit Bansal <007amitbansal@gmail.com>
2018-10-16 16:47:41 +0530

    921. Minimum Add to Make Parentheses Valid

---
+++ minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string S) {
+        int close = 0;
+        int open = 0;
+        for (char c : S) {
+            if (c == '(') {
+                open++;
+            } else if (c == ')') {
+                if (open > 0)    open--;
+                else    close++;
+            }
+        }
+
+        return (close + open);
+    }
+};
