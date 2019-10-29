852a635b7fd8e7b7a621389429393511dcd10667
Amit Bansal <007amitbansal@gmail.com>
2019-10-30 00:48:36 +0530

    1237. Find Positive Integer Solution for a Given Equation

---
+++ find-positive-integer-solution-for-a-given-equation.cpp
@@ -0,0 +1,14 @@
+// Runtime: 0 ms
+// Memory Usage: 8.4 MB
+class Solution {
+public:
+	vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+		vector<vector<int> > res;
+		int y = 1000;
+		for (int x = 1; x <= 1000; x++) {
+			while (y > 1 && customfunction.f(x, y) > z) y--;
+			if (customfunction.f(x, y) == z) res.push_back({x, y});
+		}
+		return res;
+	}
+};
